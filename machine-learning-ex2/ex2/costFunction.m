bd396f670e355d2bb85073dba465018a58978287
krishamehta <kkoolkrisha@gmail.com>
2017-04-22 16:56:04 +0530

    second commit

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -19,11 +19,12 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h = sigmoid(X*theta);
+J = -1/m* sum(y .* log(h)+(1-y) .* log(1-h));
 
 
 
-
-
+grad= grad+1/m*(X' *(h-y));
 
 
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+h = sigmoid(X*theta);
+reg = lambda/(2*m);
+temp1(1) = 0 ; 
 
+J = (-1/m  *sum (y .* log(h)+ (1-y) .* log(1-h))) +( reg *sum( theta(2:end ) .^  2 ));
 
-
-
+grad = 1/m*(X' * (h-y)) + (lambda/m)*[0;theta(2:end)];
 
 % =============================================================
 
